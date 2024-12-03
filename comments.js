f0d63b6ceee8446f1c10ad539ce85d5b120c1a5f
Heloisa do Nascimento de Almeida Magalhães <89664886+HeloMagalhaes@users.noreply.github.com>
2024-12-03 18:02:28 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,29 +1,17 @@
-// create web server and listen to port 3000
-// create a route for comments
-// create a route for comment/:id
-// create a route for comments/:id// 1. create a web server
-// 2. listen to port 3000
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-var url = require('url');
-
-var server = http.createServer(function(req, res) {
-    var pathname = url.parse(req.url).pathname;
-    var filePath = path.join(__dirname, pathname);
-
-    fs.exists(filePath, function(exists) {
-        if (exists) {
-            fs.readFile(filePath, function(err, data) {
-                res.end(data);
-            });
-        } else {
-            res.statusCode = 404;
-            res.end('404 Not Found');
-        }
-    });
+// create web server
+// create a new server
+const http = require('http');
+const server = http.createServer((req, res) => {
+  if (req.url === '/comments' && req.method === 'GET') {
+    res.write('GET request to /comments');
+    res.end();
+  } else if (req.url === '/comments' && req.method === 'POST') {
+    res.write('POST request to /comments');
+    res.end();
+  } else {
+    res.write('404 Not Found');
+    res.end();
+  }
 });
-
-server.listen(3000, function() {
-    console.log('server is listening on port 3000');
-});
+// listen on port 3000
+server.listen(3000);
